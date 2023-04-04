f1d7b2c620748f02a2705a6b301daaff3d27151e
James Collier <james.collier@vib.be>
2023-04-04 16:03:54 +0200

    Remove locking on the list of connected clients

--- server/app/client.py
+++ server/app/client.py
@@ -1,7 +1,6 @@
 " Handling of connected realtime clients "
 
 import logging
-from threading import Lock
 from typing import Literal
 from uuid import UUID, uuid4 as uuid
 
@@ -16,6 +15,7 @@ log = logging.getLogger(__name__)
 
 
 class Client:
+    "A connected client: tracks useful data for a connected client."
     id: UUID
     socket: WebSocket
     user: User
@@ -27,14 +27,17 @@ class Client:
 
     @property
     def name(self) -> str:
+        "Derive connected users real name."
         return f"{self.user.first_name} {self.user.last_name}"
 
     @property
     def role(self) -> Literal["admin"] | Literal["user"]:
+        "Derive connected users role."
         return self.user.role
 
 
 def connected(client: Client) -> Message:
+    "Construct a message to connected clients that another client has connected."
     return {
         "msg": "connected",
         "id": str(client.id),
@@ -43,45 +46,61 @@ def connected(client: Client) -> Message:
 
 
 def disconnected(client: Client) -> Message:
+    "Construct a message to connected clients that a client has disconnected."
     return {"msg": "disconnected", "id": str(client.id)}
 
 
 class ConnectionManager:
+    """
+    Tracks all connected clients and provides operations for communicating with connected clients.
+    """
+
     clients: list[Client] = []
-    lock: Lock = Lock()
 
     async def connect(self, socket: WebSocket) -> Client:
+        """
+        Add a client to the list of connected clients.
+        Perform startup handshake when a client connects to a WebSocket.
+        Return a connected client object or throw an exception.
+        """
         # Wait for a "ready" message
+        log.info("Waiting on 'ready' message")
         ready = await socket.receive_json()
-        log.info(f"Ready: {ready}")
+        log.info("Ready: %s", ready)
         # Request the bearer token.
+        log.info("Requesting bearer token")
         await socket.send_json({"msg": "auth"})
         token = await socket.receive_json()
         user = user_from_token(token["bearer"])
 
         client = Client(socket, user)
         await self.broadcast(connected(client))
-        log.info(f"connect({client.id}) waiting on lock? {self.lock.locked()}")
-        with self.lock:
-            self.clients.append(client)
 
-        log.info(f"Client {client.id} added")
+        self.clients.append(client)
+        log.info("Client %s added: %s (%s)", client.id, client.name, client.role)
 
         return client
 
     async def broadcast(self, message: Message):
-        log.info(f"broadcast() waiting on lock? {self.lock.locked()}")
-        with self.lock:
-            for client in self.clients:
-                await client.socket.send_json(message)
+        """
+        Send a message to _all_ connected clients.
+        This can throw a `WebSocketDisconnect exception when a client has closed the connection already.
+        """
+        log.info("broadcast()ing: %s", message)
+
+        for client in self.clients:
+            log.debug("Sending to %s", client.id)
+            await client.socket.send_json(message)
 
         log.info("Broadcast sent")
 
     async def disconnect(self, client: Client):
-        log.info(f"disconnect({client.id}) waiting on lock? {self.lock.locked()}")
-        with self.lock:
-            self.clients.remove(client)
-
-        log.info(f"Client {client.id} removed")
+        """
+        Remove a client from the list of connected clients.
+        NOTE: This function does _not_ close the websocket connection.
+        """
+        log.info("disconnect(%s) %s", client.id, client.name)
+        self.clients.remove(client)
+        log.info("Client %s removed", client.id)
 
         await self.broadcast(disconnected(client))
